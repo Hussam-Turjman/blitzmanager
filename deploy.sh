0f69e06a4c6086c9ce1787512c99fa3793ea7c70
Mohammed Hussam Al Turjman <hussam.turjman@gmail.com>
2021-12-22 13:09:25 +0100

    0.0.0a

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,4 @@ rm -rf blitzmanager.egg-info
 # python3 -m pip install --upgrade build
 python3 -m build
 # python3 -m pip install --upgrade twine
-# python3 -m twine upload  --repository pypi dist/*
+python3 -m twine upload  --repository pypi dist/*
